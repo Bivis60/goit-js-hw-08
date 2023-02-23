98b3022af2b7326831cb969c601a62fbbe013c02
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-02-23 22:27:11 +0200

    + д.з.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
+const genGalleryItem = galleryItems.map((item) => {
+  return (
+`<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`)
+}).join('')
+gallery.insertAdjacentHTML('beforeend', genGalleryItem);
+
+
+new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionsData:  'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+    // scrollZoom: false,
+})
 console.log(galleryItems);
