4a96828c169d538ca3ebcfc876112dd18ef5587b
Bivis60 <89808390+Bivis60@users.noreply.github.com>
2023-02-24 22:15:24 +0200

    + д.з. 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import vimeoPlayer from "@vimeo/player";
+import lodashThrottle from "lodash.throttle";
 
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+player.on('timeupdate', lodashThrottle((event) => {localStorage.setItem('videoplayer-current-time', JSON.stringify(event.seconds))}, 1000));
+
+const saveData = localStorage.getItem('videoplayer-current-time');
+const parseData = JSON.parse(saveData);
+player.setCurrentTime(parseData || 0);
